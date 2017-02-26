81786fe5a6e16109dc869bfe7f0c1072d8de5550
Tomcariello <tomcariello@gmail.com>
2017-02-26 13:29:05 -0500

    switched to single page app, added some styling, finished gallery

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 To Do:
 
+Technical:
+
+Design:
 Replace gray block on main page with high quality panoramic image. Update ABOUT box styling to compliment colors.
-Add Modal/Popup on click on the PHOTO page to see the high res version. Accompany image with text description
-Make this a single page (merge photos to index & delete the rest)
 Get more pictures & add them to the page
 Get new/improved logo to replaced the existing
 Make actual videos so something is happening in the youtube clips
-Slow down the carousel
 Add image(s)/content to make carousel more interesting
 
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,12 +1,27 @@
-$(function () {
-    $('#homeCarousel').carousel({
-        interval:2000,
+$( document ).ready(function() {
+    console.log('loaded');
+   $('#myCarousel').carousel({
+        interval: 5000,
         pause: "false"
     });
     $('#playButton').click(function () {
-        $('#homeCarousel').carousel('cycle');
+        // console.log("Play button clicked");
+        $('#myCarousel').carousel('cycle');
     });
     $('#pauseButton').click(function () {
-        $('#homeCarousel').carousel('pause');
+        // console.log("Pause button clicked");
+        $('#myCarousel').carousel('pause');
     });
+
+    //Handle Image Gallery Modals
+     $('.modalTrigger').click(function(target){
+        target.preventDefault();
+        // var targetID = $(this).data('id');
+        var imgTitle = $(this).attr('alt');
+        var imgSource = $(this).attr('src');
+        var selectedImage = "<img src='" + imgSource + "''>";
+        $('#modal-7 .modal-title').html(imgTitle);
+        $('#modal-7 .modal-body').html(selectedImage);
+        $('#modal-7').modal('show', {backdrop: 'static'});
+     });
 });
